2f4e1ecba4cc3259cf210059ffcd322c44465885
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2024-03-11 13:01:06 +0000

    Add quotes to blob storage

--- Quotes_API/function_app.py
+++ Quotes_API/function_app.py
@@ -23,18 +23,8 @@ def beautiful_quotes(req: func.HttpRequest) -> func.HttpResponse:
             type_ = req_body.get('type')
 
     if type_:
-        with open("quotes.json", "r") as f:
-            all_quotes = json.loads(f.read())
-            quote = all_quotes.get(type_)[randint(0, 19)]
-        person = ""
-        i = 0
-        while quote[len(quote) - 1 - i] != "-":
-            person += quote[len(quote) - 1 - i]
-            i += 1
-        logging.info(quote)
-        person = person[-2::-1]
-        base64_image = get_image(person)
-        response_json = {"quote": f"Here is your quote: {quote}", "image": base64_image}
+        quote, image = get_quote_image(type_)
+        response_json = {"quote": f"Here is your quote: {quote}", "image": image}
         response_body = json.dumps(response_json)
         return func.HttpResponse(
             body=response_body,
@@ -49,16 +39,26 @@ def beautiful_quotes(req: func.HttpRequest) -> func.HttpResponse:
         )
     
  
-def get_image(person):
+def get_quote_image(type_):
     connect_str = os.environ["AzureWebJobsStorage"]
     container_name = "data"
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     container_client = blob_service_client.get_container_client(container_name)
+    blob_client1 = container_client.get_blob_client("quotes.json")
+    stream1 = blob_client1.download_blob()
+    quotes = json.loads(stream1.readall())
+    quote = quotes.get(type_)[randint(0, 19)]
+    person = ""
+    i = 0
+    while quote[len(quote) - 1 - i] != "-":
+        person += quote[len(quote) - 1 - i]
+        i += 1
+    person = person[-2::-1]
     blob_list = list(container_client.list_blobs(name_starts_with=f"images/{person}"))
     rand_image = blob_list[randint(0, len(blob_list) - 1)]
-    blob_client = container_client.get_blob_client(rand_image)
-    stream = blob_client.download_blob()
-    image = base64.b64encode(stream.readall()).decode('utf-8')
-    return image
+    blob_client2 = container_client.get_blob_client(rand_image)
+    stream2 = blob_client2.download_blob()
+    image = base64.b64encode(stream2.readall()).decode('utf-8')
+    return quote, image
 
     
